e0447a7c2d88bfb2785b653f894feee554b2a734
DaniCode122 <danielperezbedoya382@gmail.com>
2024-08-05 15:52:19 -0500

    pantalla home hecha

---
+++ js/typed.js
@@ -0,0 +1,22 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function () {
+    var typed = new Typed('#typed', {
+        strings: [
+            'Bienvenidos a OMNICOM.',
+            'Soluciones Energéticas Integrales y Sostenibles.',
+            'Proyectos exitosos en Colombia y Virginia.'
+        ],
+        typeSpeed: 50,     
+        backSpeed: 50,  
+        backDelay: 1500,   
+        startDelay: 1000,  
+        loop: true,         
+        loopCount: Infinity,
+        showCursor: true,   
+        cursorChar: '|',   
+        smartBackspace: true, 
+        fadeOut: true,    
+        fadeOutClass: 'typed-fade-out',  
+        fadeOutDelay: 500, 
+    });
+});
